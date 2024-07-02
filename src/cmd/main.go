e4d9a0daf193999deda557e2d8e58a77362fe3a2
muizu555 <muizu0920555@protonmail.com>
2024-07-03 01:55:37 +0900

    fin step1

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -17,18 +17,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	file, err := os.Open("trade_history.csv")
+	// trade_history.csvファイルを開く
+	file1, err := os.Open("trade_history.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer file.Close()
+	defer file1.Close()
 
-	r := csv.NewReader(file)
-	rows, err := r.ReadAll() // csvを一度に全て読み込む
+	// CSVリーダーを作成
+	r1 := csv.NewReader(file1)
+	rows1, err := r1.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// reference_prices.csvファイルを開く
+	file2, err := os.Open("reference_prices.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file2.Close()
+
+	// CSVリーダーを作成
+	r2 := csv.NewReader(file2)
+	rows2, err := r2.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 環境変数から接続情報を取得
 	database := os.Getenv("DATABASE")
 	userName := os.Getenv("USERNAME")
 	userPass := os.Getenv("USERPASS")
@@ -36,19 +53,32 @@ func main() {
 	// DSNを構築
 	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", userName, userPass, database)
 
+	// MySQLデータベースに接続
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// テーブルにデータを挿入
-	insertStmt := "INSERT INTO TradeHistory (UserID, FundID, Quantity, TradeDate) VALUES (?, ?, ?, ?)"
-	for i, row := range rows {
+	// TradeHistoryテーブルにデータを挿入
+	insertStmt1 := "INSERT INTO TradeHistory (UserID, FundID, Quantity, TradeDate) VALUES (?, ?, ?, ?)"
+	for i, row := range rows1 {
 		if i == 0 {
 			continue
 		}
-		_, err := db.Exec(insertStmt, row[0], row[1], row[2], row[3])
+		_, err := db.Exec(insertStmt1, row[0], row[1], row[2], row[3])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// ReferencePricesテーブルにデータを挿入
+	insertStmt2 := "INSERT INTO ReferencePrices (FundID, ReferencePriceDate, ReferencePrice) VALUES (?, ?, ?)"
+	for i, row := range rows2 {
+		if i == 0 {
+			continue
+		}
+		_, err := db.Exec(insertStmt2, row[0], row[1], row[2])
 		if err != nil {
 			log.Fatal(err)
 		}
